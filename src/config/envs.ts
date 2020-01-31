5084103c9de87143308c0cea2cfb5fe7543945b6
Johnny Vanhentenryk <johnny.vanhentenryk@bnpparibasfortis.com>
2020-01-31 17:10:34 +0100

    Update props for TST

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -10,12 +10,12 @@ const envs = {
     state: 'state'
   },
   TST: {
-    authURL: 'https://sandbox.auth-dev.tstbnpparibasfortis.com',
+    authURL: 'https://sandbox.auth.tstbnpparibasfortis.com',
     apiURL: 'http://127.0.0.1:9000',
-    organization: '22870d49-f7f0-463a-8bb8-d6d98ffc5a05',
+    organization: '3d2aca7f-58e4-45cb-ba8f-c43561922086',
     stetVersion: '1.4.0.47.develop',
-    clientId: '88562b87-c5ec-4ea2-b40e-82a7f203d785',
-    clientSecret: '14935daa2872144d192a5ee6efb1002a0574365932d05a6a127089c75aad2d08d773ad08fcec0247c13ceb3e1fa531f4',
+    clientId: 'f0c717f9-3083-43ec-bb2e-cf08dfa1ba46',
+    clientSecret: '1e724d1012d408734fdcade522ffa4b65fba2adf933bdb6d191dec85cb0b53b3cb9d2d75912ce0826d484669f9c472fe',
     redirectURI: 'http://127.0.0.1:8090/landing',
     state: 'state'
   },
