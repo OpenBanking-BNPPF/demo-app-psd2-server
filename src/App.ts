af04c70038f66312bca3ddde41a1527137558497
Johnny Vanhentenryk <johnny.vanhentenryk@bnpparibasfortis.com>
2021-03-22 08:43:32 +0100

    Conf pre-commit

--- src/App.ts
+++ src/App.ts
@@ -37,7 +37,7 @@ class App {
             })
         });
 
-        this.app.use((err: Error & { status: number }, request: express.Request, response: express.Response, next: express.NextFunction): void => {
+        this.app.use((err: Error & { status: number }, request: express.Request, response: express.Response): void => {
             response.status(err.status || 500);
             response.json({
                 error: "Server error"
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-const {envs} = require('./envs')
+import {envs} from './envs'
 const e = process.env.SANDBOX_ENV ? process.env.SANDBOX_ENV : 'PROD'
 console.log('Loading config for ', e)
 const appConfig = envs[e]
